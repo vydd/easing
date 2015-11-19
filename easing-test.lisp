7d3e939a350c323d338e830623d23fc85c31272b
vydd <vidovicd@gmail.com>
2015-11-19 16:30:03 +0100

    Sketch compatibility

--- easing-test.lisp
+++ easing-test.lisp
@@ -7,7 +7,7 @@
       (/ (1+ (truncate (* x 10))) 10)
       (/ (1+ (truncate (* x 4))) 4)))
 
-(defsketch ease-test (:title "Easing" :width 1200 :height 600)
+(defsketch ease-test (:title "Easing" :width 1200 :height 600 :debug :scancode-f1)
     ((rows 4)
      (cols 8)
      (frames 0))
@@ -30,21 +30,22 @@
       (dotimes (y rows)
 	(dotimes (x cols)
 	  (with-identity-matrix
-	    (ntranslate (* x (/ width cols)) (* y (/ height rows)))
-	    (nscale (/ width cols) (/ height rows))
+	    (ntranslate 0.1 0.3)
+	    (nscale (* 0.8 (/ width cols)) (* 0.6 (/ height rows)))
+	    (ntranslate (* (/ width cols) x) (* (/ height rows) y))
 	    (with-pen (make-pen :fill (gray 1))
-	      (rect 0.1 0.2 0.9 0.8))
-	    (ntranslate 0.1 0.8)
-	    (nscale 0.8 -0.6)
+	      (rect 0 0 1 1))
 	    (when (consp funcs)
 	      (draw-easing (car funcs) frames)
 	      (setf funcs (cdr funcs)))))))))
 
 (defun draw-easing (fn frames)
-  (loop for i from 0 upto 0.99 by 0.01 do
-       (line i (funcall fn i) (+ i 0.01) (funcall fn (+ i 0.01))))
+  (let ((step 0.01))
+    (with-pen (make-pen :stroke (gray 0) :weight 0.01)
+      (loop for i from 0 upto 0.99 by step do
+	   (line i (- 1.0 (funcall fn i)) (+ i step) (- 1.0 (funcall fn (+ i step)))))))
   (with-pen (make-pen :fill (rgb 1 0 0))
-    (ellipse 1.0 (funcall fn (/ frames 100)) 0.04 0.04)))
+    (ellipse 1.0 (- 1.0 (funcall fn (/ frames 100))) 0.04 0.04)))
 
 (defun test ()
   (make-instance 'ease-test))

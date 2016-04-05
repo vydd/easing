c18003dc6d773d19126e79db7b4677825ea026a9
vydd <vidovicd@gmail.com>
2016-04-05 19:13:27 +0200

    Updates the test
    It now runs with the most recent version of Sketch. The example also
    shows some of the soon to be resolved issues with Sketch's line drawing.

--- easing-test.lisp
+++ easing-test.lisp
@@ -7,45 +7,45 @@
       (/ (1+ (truncate (* x 10))) 10)
       (/ (1+ (truncate (* x 4))) 4)))
 
-(defsketch ease-test (:title "Easing" :width 1200 :height 600 :debug :scancode-f1)
+(defparameter *easings*
+  '(linear
+    ease-in-sine ease-out-sine ease-in-out-sine
+    ease-in-cubic ease-out-cubic ease-in-out-cubic
+    ease-in-quad ease-out-quad ease-in-out-quad
+    ease-in-quart ease-out-quart ease-in-out-quart
+    ease-in-quint ease-out-quint ease-in-out-quint
+    ease-in-exp ease-out-exp ease-in-out-exp
+    ease-in-circ ease-out-circ ease-in-out-circ
+    ease-in-elastic ease-out-elastic ease-in-out-elastic
+    ease-in-back ease-out-back ease-in-out-back
+    ease-in-bounce ease-out-bounce ease-in-out-bounce
+    ease-in-custom))
+
+(defsketch ease-test (:title "Easing" :width 1200 :height 600 :debug :scancode-f1 :y-axis :up)
     ((rows 4)
      (cols 8)
      (frames 0))
   (background (gray 0.8))
   (setf frames (mod (1+ frames) 100))
   (with-pen (make-pen :stroke (gray 0))
-    (let ((funcs 	   
-	   '(linear
-	     ease-in-sine ease-out-sine ease-in-out-sine
-	     ease-in-cubic ease-out-cubic ease-in-out-cubic
-	     ease-in-quad ease-out-quad ease-in-out-quad
-	     ease-in-quart ease-out-quart ease-in-out-quart
-	     ease-in-quint ease-out-quint ease-in-out-quint
-	     ease-in-exp ease-out-exp ease-in-out-exp
-	     ease-in-circ ease-out-circ ease-in-out-circ
-	     ease-in-elastic ease-out-elastic ease-in-out-elastic
-	     ease-in-back ease-out-back ease-in-out-back
-	     ease-in-bounce ease-out-bounce ease-in-out-bounce
-	     ease-in-custom)))
-      (dotimes (y rows)
-	(dotimes (x cols)
-	  (with-identity-matrix
-	    (ntranslate 0.1 0.3)
-	    (nscale (* 0.8 (/ width cols)) (* 0.6 (/ height rows)))
-	    (ntranslate (* (/ width cols) x) (* (/ height rows) y))
-	    (with-pen (make-pen :fill (gray 1))
-	      (rect 0 0 1 1))
-	    (when (consp funcs)
-	      (draw-easing (car funcs) frames)
-	      (setf funcs (cdr funcs)))))))))
+    (loop for fn in *easings*
+       for x = 0 then (mod (1+ x) cols)
+       for y = 0 then (+ y (if (zerop x) 1 0))
+       do (progn
+	    (with-identity-matrix
+	      (translate (* x (/ width cols)) (* y (/ height rows)))
+	      (scale (/ width cols) (/ height rows))
+	      (translate 0.1 0.2)
+	      (scale 0.8 0.6)
+	      (draw-easing fn frames))))))
 
 (defun draw-easing (fn frames)
-  (let ((step 0.01))
-    (with-pen (make-pen :stroke (gray 0) :weight 0.01)
-      (loop for i from 0 upto 0.99 by step do
-	   (line i (- 1.0 (funcall fn i)) (+ i step) (- 1.0 (funcall fn (+ i step)))))))
-  (with-pen (make-pen :fill (rgb 1 0 0))
-    (ellipse 1.0 (- 1.0 (funcall fn (/ frames 100))) 0.04 0.04)))
+  (with-pen (make-pen :fill (gray 1 0.8) :stroke (gray 0 0.2) :weight 0.01)
+    (rect 0 0 1 1))
+  (with-pen (make-pen :stroke +black+ :weight 0.01)
+    (apply #'polyline (loop for i upto 0.99 by 0.01 append (list i (funcall fn i)))))
+  (with-pen (make-pen :fill +red+)
+    (circle 1.0 (funcall fn (/ frames 100)) 0.04)))
 
 (defun test ()
   (make-instance 'ease-test))
